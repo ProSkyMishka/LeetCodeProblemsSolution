2f26c05d836dd943e8fd763eb8af2b43a24479d2
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-07-30 16:26:35 +0300

    Update solution.swift

--- 1.Two Sum(easy)/solution.swift
+++ 1.Two Sum(easy)/solution.swift
@@ -1,1 +1,13 @@
-
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<(nums.count - 1) {
+            for j in (i + 1)..<nums.count {
+                var sum = nums[i] + nums[j]
+                if sum == target {
+                    return [i, j]
+                }
+            }
+        }
+        return [0, 0]
+    }
+}
