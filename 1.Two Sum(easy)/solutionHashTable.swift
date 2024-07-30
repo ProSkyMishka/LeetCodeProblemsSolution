59f84d2f4057847774b24ef1d70785261f722578
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2024-07-30 16:36:08 +0300

    Update solutionHashTable.swift

--- 1.Two Sum(easy)/solutionHashTable.swift
+++ 1.Two Sum(easy)/solutionHashTable.swift
@@ -1,1 +1,16 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var hashTable: [Int: Int] = [:]
 
+        for i in 0..<nums.count {
+            var res = target - nums[i]
+            
+            if let j = hashTable[res] {
+                return [j, i]
+            }
+            
+            hashTable[nums[i]] = i
+        }
+        return [0, 0]
+    }
+}
